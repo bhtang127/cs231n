0f1763396b5a0235800c1566c054750beb308e42
bhtang127 <bhtang127@gmail.com>
2017-09-24 00:10:14 -0400

    a1 svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,13 +35,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i]
+        dW[:, y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -70,11 +74,25 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  
+  relative_scores = X.dot(W) - np.matrix(np.sum( X * W[:,y].T, axis=1)).T + 1
+  losses = np.maximum(relative_scores, 0)
+  loss += np.sum(losses)
+  loss /= num_train
+  loss -= 1
+
+  loss += reg * np.sum(W * W)
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-
+  is_correct_class = (np.matrix(y).T == np.matrix(range(num_classes)))
+  aux = np.multiply(is_correct_class, np.sum(relative_scores > 0, axis=1))
+  dW += np.dot( np.matrix(X).T , (relative_scores > 0) - aux)
+  dW /= num_train
+  dW += 2 * reg * W 
 
   #############################################################################
   # TODO:                                                                     #
